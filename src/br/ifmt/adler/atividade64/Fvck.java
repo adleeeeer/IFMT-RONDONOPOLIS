ea9a2b3bfc019ed6f5ac3f090d2706d49f0bbd90
Administrator <Administrator@172.31.14.31>
2018-06-16 13:11:41 -0300

    Upload

---
+++ src/br/ifmt/adler/atividade64/Fvck.java
@@ -0,0 +1,37 @@
+package br.ifmt.adler.atividade64;
+
+/**
+ * Copyright (C) Adler Lopes, all rights reserved unauthorized copying of this
+ * file, via any medium is strictly prohibited proprietary and confidential
+ * 
+ * eu@adlerlopes.com.br adlerlopes.com.br
+ * 
+ * C O P I A N A O C O M E D I A
+ */
+
+public class Fvck {
+
+	public static void main(String[] args) {
+
+		System.out.println("----------------------------------------------------------");
+		System.out.println("                IFMT - CAMPUS RONDONOPOLIS                ");
+		System.out.println("                                                          ");
+		System.out.println("             Algoritmo e lógica da programação            ");
+		System.out.println("                                                          ");
+		System.out.println("                      Atividade 74                        ");
+		System.out.println("----------------------------------------------------------");
+
+		for (int i = 0; i <= 10; i++) {
+
+			System.out.println("----------------------------------------------------------");
+			System.out.println("                Tabuada do número: " + i + "              ");
+			System.out.println("----------------------------------------------------------");
+
+			for (int ix = 0; ix <= 10; ix++) {
+				System.out.println("" + i + "*" + ix + " resposta " + (i * ix));
+			}
+
+		}
+	}
+
+}
